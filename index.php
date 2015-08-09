6518bd23a78212d8a5f1b6892151d37b78e5c198
John Nannas <jwnannas@gmail.com>
2015-08-09 14:02:31 -0400

    Initial setup of version control and creation of test page and readme

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+		<title>WeatherDrive</title>
+		<meta charset="utf-8">
+
+		<link rel='stylesheet' href='style.css'>
+</head>
+<body>
+		<h1>Hello World!</h1>
+</body>
+</html>
+
