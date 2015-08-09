0c945b5e6c56c1bc5ec4a60655c2d71cfb7e10cc
John Nannas <jwnannas@gmail.com>
2015-08-09 19:46:45 -0400

    basic page build, added working responsive google map with bootstrap

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-		<title>WeatherDrive</title>
-		<meta charset="utf-8">
-
-		<link rel='stylesheet' href='style.css'>
+	<title>WeatherDrive</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCs1a-vtgkgXnsIdVGldol_v5IrRJHcpR4">
+    </script>
+    <script type="text/javascript">
+    	function initialize() {
+        	var mapOptions = {
+          		center: { lat: 42.37469, lng: -71.12085},
+          		zoom: 18
+        	};
+        	var map = new google.maps.Map(document.getElementById('map-canvas'),
+            	mapOptions);
+      	}
+     	google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 </head>
 <body>
-		<h1>Hello World</h1>
+	<header><h1>WeatherDrive Header</h1></header>
+	<p  class="lead">INFO PANE</p>
+	<div class="map-container">
+		<div id="map-canvas" class="map-canvas"></div>
+	</div>
 </body>
 </html>
 
