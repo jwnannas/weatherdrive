918d4ffe515996d33149130f55a48b2c08e3bb1a
John Nannas <jwnannas@gmail.com>
2015-10-10 00:11:06 -0400

    revised email message to send results

--- mail.php
+++ mail.php
@@ -1,5 +1,7 @@
 <?php
 	$url = $_POST['weatherDriveURL'];
 	$email = $_POST['email'];
-	mail($email, 'WeatherDrive Results', $url);
+	$message = "Thanks for using WeatherDrive! Your WeatherDrive search can be accessed here:\r\n".$url."\r\nHave a safe trip.\r\nSincery,\r\nThe WeatherDrive Team";
+	$headers = 'From: weatherdrive@weatherdrive.org';
+	mail($email, 'WeatherDrive Results', $message, $headers);
 ?>
