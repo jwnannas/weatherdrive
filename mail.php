b7b781746c06a1e369ea94bc24ad26581f1702ce
John Nannas <jwnannas@gmail.com>
2015-10-10 00:35:35 -0400

    fixed typos in mail message

--- mail.php
+++ mail.php
@@ -1,7 +1,6 @@
 <?php
 	$url = $_POST['weatherDriveURL'];
 	$email = $_POST['email'];
-	$message = "Thanks for using WeatherDrive! Your WeatherDrive search can be accessed here:\r\n".$url."\r\nHave a safe trip.\r\nSincery,\r\nThe WeatherDrive Team";
-	$headers = 'From: weatherdrive@weatherdrive.org';
-	mail($email, 'WeatherDrive Results', $message, $headers);
+	$message = "Thanks for using WeatherDrive. Your WeatherDrive search can be accessed here:\r\n".$url."\r\nHave a safe trip!\r\n\r\nSincerely,\r\nThe WeatherDrive Team";
+	mail($email, 'WeatherDrive Results', $message);
 ?>
