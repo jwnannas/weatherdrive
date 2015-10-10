55e3346494aee851609dd0efdafa257011e87667
John Nannas <jwnannas@gmail.com>
2015-10-10 00:50:28 -0400

    additional change to mail

--- mail.php
+++ mail.php
@@ -2,5 +2,6 @@
 	$url = $_POST['weatherDriveURL'];
 	$email = $_POST['email'];
 	$message = "Thanks for using WeatherDrive. Your WeatherDrive search can be accessed here:\r\n".$url."\r\nHave a safe trip!\r\n\r\nSincerely,\r\nThe WeatherDrive Team";
-	mail($email, 'WeatherDrive Results', $message);
+	$headers = 'From: weatherdrive@weatherdrive.org';
+	mail($email, 'WeatherDrive Results', $message, $headers);
 ?>
